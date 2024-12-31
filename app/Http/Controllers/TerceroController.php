0fb31268a2d948a31c0c3ab69812db8e0dc5c982
ipedraza <ingprogramar2@arrivesafe.co>
2024-12-31 18:46:25 -0500

    correction function array

--- app/Http/Controllers/TerceroController.php
+++ app/Http/Controllers/TerceroController.php
@@ -500,8 +500,9 @@ class TerceroController extends Controller
         $key = '';
         $pattern = '1234567890abcdef';
         $max = strlen($pattern) - 1;
-        for ($i = 0; $i < $longitud; $i++) $key .= $pattern{
-            mt_rand(0, $max)};
+        for ($i = 0; $i < $longitud; $i++) {
+            $key .= $pattern[mt_rand(0, $max)];
+        }
         return $key;
     }
 
